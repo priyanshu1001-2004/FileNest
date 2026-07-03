a38428a74c0694101e95bde9667844a34de48b5b
priyanshu1001-2004 <priyanshugupta4005@gmail.com>
2026-07-03 14:38:00 +0530

    buyer middeware updated

--- app/Http/Middleware/BuyerMiddleware.php
+++ app/Http/Middleware/BuyerMiddleware.php
@@ -15,9 +15,14 @@ class BuyerMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->check() && auth()->user()->isBuyer()) {
+         if (auth()->check() && auth()->user()->isBuyer()) {
+            if (!auth()->user()->status) {
+                abort(403, 'Your buyer profile account is suspended or pending approval.');
+            }
             return $next($request);
         }
         abort(403, 'Unauthorized access.');
+
+       
     }
 }
